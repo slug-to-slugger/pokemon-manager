5526c87d345a236a4c7759984e19b67d14dccebf
Yoshiya Watanabe <ywatanab@mail.ccsf.edu>
2020-02-16 20:03:55 +0900

    change db from sqlite to mysql

--- pokemon_manager/pokemon_manager/settings.py
+++ pokemon_manager/pokemon_manager/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'pokemon_manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pokemon_manager',
+        'USER': 'pokemon', # ログインユーザー名
+        'PASSWORD': 'pokemon',
+        'HOST': 'pokemon_db',
+        'PORT': 3306, # default
     }
 }
 
