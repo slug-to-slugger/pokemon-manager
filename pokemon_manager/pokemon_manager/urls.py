0588955c13557987c5570d5f9d826382cfc4fc53
Yoshiya Watanabe <ywatanab@mail.ccsf.edu>
2020-02-19 22:28:48 +0900

    urlsのpathを変更

--- pokemon_manager/pokemon_manager/urls.py
+++ pokemon_manager/pokemon_manager/urls.py
@@ -3,8 +3,8 @@ from pokemon import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('trainer/', views.TrainerList.as_view()),
-    path('trainer/<int:pk>', views.TrainerDetail.as_view())
+    path('trainers/', views.TrainerList.as_view()),
+    path('trainer/<int:pk>/', views.TrainerDetail.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns) # これ要る?
