5fc0cd96c1b76dda3eb6f02af4f5a9fb5245737c
Yoshiya Watanabe <ywatanab@mail.ccsf.edu>
2020-02-19 20:37:55 +0900

    シリアライザのクラス名を分かり易く変更

--- pokemon_manager/pokemon/serializer.py
+++ pokemon_manager/pokemon/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializer
 
-class Trainer(serializer.ModelSerializer):
+class TrainerSerializer(serializer.ModelSerializer):
     class Meta:
         model = Trainer
         field = ['id','created','name','password','user_id']
