3c25e799b1062fe74cd9dd10083016ddbabfbbf9
Yoshiya Watanabe <ywatanab@mail.ccsf.edu>
2020-02-21 21:48:54 +0900

    added pokemon table and etc

--- pokemon_manager/pokemon/serializer.py
+++ pokemon_manager/pokemon/serializer.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from pokemon.models import Trainer
+from pokemon.models import Trainer, Pokemon
 
 class TrainerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Trainer
         fields = ['id','created','name','password','user_id']
+
+class PokemonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pokemon
+        # fields = ['name', 'special_skill', ]s
+        fields = '__all__'
