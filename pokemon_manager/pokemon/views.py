0be08189b13475d49fbc136d43d4e558c9085333
Yoshiya Watanabe <ywatanab@mail.ccsf.edu>
2020-02-21 21:48:54 +0900

    added pokemon table and etc

--- pokemon_manager/pokemon/views.py
+++ pokemon_manager/pokemon/views.py
@@ -1,6 +1,7 @@
-from pokemon.models import Trainer
-from pokemon.serializer import TrainerSerializer
+from pokemon.models import Trainer, Pokemon
+from pokemon.serializer import TrainerSerializer, PokemonSerializer
 from rest_framework import generics
+import logging
 
 class TrainerList(generics.ListCreateAPIView):
     queryset = Trainer.objects.all()
@@ -9,3 +10,23 @@ class TrainerList(generics.ListCreateAPIView):
 class TrainerDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Trainer.objects.all()
     serializer_class = TrainerSerializer
+    # putメソッドをオーバーライド
+    def put(self, request, *args, **kwargs):
+        # request.dataの中身をログに出力
+        logger = logging.getLogger('development')
+        logger.info(request.data)
+        return self.update(request, *args, **kwargs)
+
+class PokemonList(generics.ListCreateAPIView):
+    queryset = Pokemon.objects.all()
+    serializer_class = PokemonSerializer
+
+class PokemonDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Pokemon.objects.all()
+    serializer_class = PokemonSerializer
+    # putメソッドをオーバーライド
+    def put(self, request, *args, **kwargs):
+        # request.dataの中身をログに出力
+        logger = logging.getLogger('development')
+        logger.info(request.data)
+        return self.update(request, *args, **kwargs)
