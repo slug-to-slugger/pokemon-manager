c86fd8bd6bef3bf722d00b74ef926e8aace3d24b
Yoshiya Watanabe <ywatanab@mail.ccsf.edu>
2020-02-23 05:52:39 +0900

    属性が降順になるように

--- pokemon_manager/pokemon/serializer.py
+++ pokemon_manager/pokemon/serializer.py
@@ -1,13 +1,26 @@
 from rest_framework import serializers
-from pokemon.models import Trainer, Pokemon
+from pokemon.models import Trainer, Partner
+import logging
+logger = logging.getLogger('development')
 
 class TrainerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Trainer
         fields = ['id','created','name','password','user_id']
 
-class PokemonSerializer(serializers.ModelSerializer):
+class PartnerSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Pokemon
-        # fields = ['name', 'special_skill', ]s
+        model = Partner
         fields = '__all__'
+
+    def to_internal_value(self, data):
+        # 属性1が属性2より小さければ、逆にする
+        if data.get('attribute1') > data.get('attribute2'):
+            validated = {
+                        'attribute1': data.get('attribute2'),
+                        'attribute2': data.get('attribute1'),
+                        }
+        else:
+            validated = {}
+        logging.warning(validated)
+        return validated
