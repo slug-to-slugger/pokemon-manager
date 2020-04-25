9185bf5e7f092439c4c19b8622ef203317fbf3d3
Yoshiya Watanabe <ywatanab@mail.ccsf.edu>
2020-04-25 20:19:08 +0900

    delete unneeded host from CORS_ORIGIN_WHITELIST = (

--- backend/app/pokemon_manager/settings.py
+++ backend/app/pokemon_manager/settings.py
@@ -152,9 +152,6 @@ AUTH_USER_MODEL = 'pokemon.Trainer'
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://localhost:3000/',
     'http://127.0.0.1:3000',
-    'http://127.0.0.1:3000/',
     'http://pokemon-nginx:3000',
-    'http://pokemon-nginx:3000/',
 )
