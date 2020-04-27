82d57dd5d12beca7ea28f8c45ecc9fe6f4bd1485
Yoshiya Watanabe <ywatanab@mail.ccsf.edu>
2020-04-28 06:07:10 +0900

    enable to get token

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,46 +3,87 @@ import React, { Component } from 'react'
 class App extends Component {
   state = {
     data: [],
+    token: [],
   }
 
   // Code is invoked after the component is mounted/inserted into the DOM tree.
   componentDidMount() {
-    const url =
-      'http://localhost/trainers/'
+    // トレーナー登録
+    // const url = 'http://localhost/trainers/';
+    // const method = "POST";
+    // const headers = {
+    //   'Accept': 'application/json',
+    //   'Content-Type': 'application/json'
+    // };
+    // const obj = {"username": "test2@example.com", "password": "test", "nickname": "Jiro"};
+    // const body = JSON.stringify(obj);
+    // fetch(url,{method, headers, body})
+    // .then((result) => {
+    //   return result.json()
+    // }).then((result) => {      
+    //   console.log(result);
+    // })
+    // .catch((error) => {
+    //   console.error('Error:', error);
+    // });
 
-    fetch(url,{
-      // method:"POST"
-      })
-      .then((result) => {
-        return result.json()
-      }).then((result) => {      
-        console.log(result);
-        this.setState({
-          data: result,
-        })
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-    // const result = fetch(url);
-    // console.log("result of fetch");
-    // console.log(result);
-  }
+    // 全トレーナーデータ取得
+    // fetch(url)
+    // .then((result) => {
+    //   return result.json()
+    // }).then((result) => {
+    //   console.log(result);
+    //     this.setState({
+    //       data: result,
+    //     })
+    // })
+    // .catch((error) => {
+    //   console.error('Error:', error);
+    // });
 
-
-  render() {
-    const { data } = this.state
-    
-    console.log(data);
-    console.log(this.state)
-
-    const result = data.map((d) => {
-      return Object.keys(d).map((key) => {
-        return <li key={key}>{d[key]}</li>
+    // トークン情報取得
+    // curl -H "Content-Type: application/json" http://localhost/api-token-auth/ -d '{"username": "test@example.com", "password": "test"}'
+    const url = 'http://localhost/api-token-auth/';
+    const method = "POST";
+    const headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    };
+    const obj = {"username": "test@example.com", "password": "test"};
+    const body = JSON.stringify(obj);
+    fetch(url,{method, headers, body})
+    .then((result) => {
+      return result.json()
+    })
+    .then((result) => {      
+      console.log(result);
+      this.setState({
+        token: result,
       })
     })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  }
 
-    return <ul>{result}</ul>
+  // render() {
+  //   const { data } = this.state
+    
+  //   const result = data.map((d) => {
+  //     return Object.keys(d).map((key) => {
+  //       return <li key={key}>{d[key]}</li>
+  //     })
+  //   })
+
+  //   return <ul>{result}</ul>
+  // }
+
+  // トークン取得
+  render() {
+    // console.log(this.state);
+    const { token } = this.state
+  return <div>{token.token}</div>
+
   }
 }
 
